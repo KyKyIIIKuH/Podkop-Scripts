15663cbe401dc869ec11c72470443504e0bd22a2
Denis <7987636+KyKyIIIKuH@users.noreply.github.com>
2026-06-07 23:02:12 +0300

    Update headers in subs_no_idn.sh for wget command

--- subs_no_idn.sh
+++ subs_no_idn.sh
@@ -26,7 +26,7 @@ restart_target() {
 
 # --- Получаем список и декодируем base64 ---
 VLESS_LIST=$(wget --no-check-certificate \
---header="User-Agent: v2rayNG/1.10.5" \
+--header="User-Agent: v2rayNG" \
 --header="x-hwid: openwrt-router-001" \
 --header="x-device-os: OpenWRT" \
 --header="x-device-model: OpenWrt Router" \
